a07773a418082519a189e336ece7542da403dc50
pphila <efl2ice89@gmail.com>
2022-12-10 16:56:16 -0800

    add passing test to Vendor class constructor for name and description

--- BakeryOrder.Tests/ModelTests/VendorTests.cs
+++ BakeryOrder.Tests/ModelTests/VendorTests.cs
@@ -15,6 +15,34 @@ namespace BakeryOrder.Tests
       Vendor newVendor = new Vendor("Rick", "Multiverse cafe owner");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
-    
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Name";
+      Vendor newVendor = new Vendor(name, "desctription");
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetDesctription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Cafe and taproom";
+      Vendor newVendor = new Vendor("Ricks Cafe", description);
+
+      //Act
+      string result = newVendor.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
   }
 }
