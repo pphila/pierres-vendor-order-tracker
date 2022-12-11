09328c1a258348e6b8c77805039ed90c23b71ea3
pphila <efl2ice89@gmail.com>
2022-12-10 19:54:57 -0800

    add vendor route logic for order form and WIP for vendor cshtml

--- BakeryOrder/Controllers/VendorsController.cs
+++ BakeryOrder/Controllers/VendorsController.cs
@@ -20,5 +20,46 @@ namespace BakeryOrder.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int Id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription, int bread1, int bread2, int bread3, int bread4, int pastry1, int pastry2, int pastry3, int pastry4, DateTime orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      string venderName = foundVendor.Name;
+      
+      int breadTotal = bread1 + bread2 + bread3 + bread4;
+      int pastryTotal = pastry1 + pastry2 + pastry3 + pastry4;
+      string dateToDisplay = orderDate.ToShortDateString();
+      string orderTitle = venderName + " " + dateToDisplay;
+
+      Order newOrder = new Order(orderTitle, orderDescription, breadTotal, pastryTotal, dateToDisplay);
+      newOrder.CalculatePrice();
+
+      foundVendor.AddItem(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
+
   }
 }
